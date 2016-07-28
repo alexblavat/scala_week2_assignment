7c5443cb5d312a5c199c3c112906bee493c109d0
Oleksiy Blavat <blavat.alex@gmail.com>
2016-07-28 15:01:17 -0400

    more funsets

--- src/main/scala/funsets/FunSets.scala
+++ src/main/scala/funsets/FunSets.scala
@@ -68,12 +68,12 @@ object FunSets {
     * Returns whether there exists a bounded integer within `s`
     * that satisfies `p`.
     */
-  def exists(s: Set, p: Int => Boolean): Boolean = ???
+  def exists(s: Set, p: Int => Boolean): Boolean = !forall(s, e => !p(e))
 
   /**
     * Returns a set transformed by applying `f` to each element of `s`.
     */
-  def map(s: Set, f: Int => Int): Set = ???
+  def map(s: Set, f: Int => Int): Set = (y => exists(s, x => y == f(x)))
 
   /**
     * Displays the contents of a set
